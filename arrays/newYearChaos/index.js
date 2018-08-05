8da2e5980c02c810d52d4d20597250593cff849c
TB831 <druu831@gmail.com>
2018-08-04 17:15:08 -0700

    setup files for newYearChaos

---
+++ arrays/newYearChaos/index.js
@@ -0,0 +1,8 @@
+// --- Directions
+// Hackerrank problem: New Year Chaos
+// link: https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+// Complete the minimumBribes function below.
+function minimumBribes(q) {
+  
+}
