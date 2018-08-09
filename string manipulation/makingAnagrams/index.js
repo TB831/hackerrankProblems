a40dd2cc6dc6593c17acea211fe9b147a1b217e7
TB831 <druu831@gmail.com>
2018-08-09 10:09:23 -0700

    makingAnagrams complete

---
+++ string manipulation/makingAnagrams/index.js
@@ -0,0 +1,7 @@
+// --- Directions
+// Hackerrank Problem: Making Anagrams
+// link: https://www.hackerrank.com/challenges/ctci-making-anagrams/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+
+function makeAnagrams(a, b) {
+
+}
