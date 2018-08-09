d116d81c8a50258fdcd06c2ebf43a44a948d2350
TB831 <druu831@gmail.com>
2018-08-09 10:21:37 -0700

    alternatingCharacters setup

---
+++ string manipulation/alternatingCharacters/index.js
@@ -0,0 +1,7 @@
+// --- Directions
+// Hackerranks problem:
+// link: https://www.hackerrank.com/challenges/alternating-characters/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+
+function alternatingCharacters(s) {
+
+}
