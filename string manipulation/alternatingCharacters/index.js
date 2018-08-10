f71a5ccf286e64b2f84aa34ef06a29fae72ebe1d
TB831 <druu831@gmail.com>
2018-08-10 11:10:29 -0700

    alternatingCharacters complete

--- string manipulation/alternatingCharacters/index.js
+++ string manipulation/alternatingCharacters/index.js
@@ -3,5 +3,12 @@
 // link: https://www.hackerrank.com/challenges/alternating-characters/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
 
 function alternatingCharacters(s) {
-
+  let count = 0;
+  s = s.split('');
+  for (let i=0; i<s.length; i++) {
+    if (s[i] === s[i+1]) {
+      count++;
+    }
+  }
+  return count;
 }
