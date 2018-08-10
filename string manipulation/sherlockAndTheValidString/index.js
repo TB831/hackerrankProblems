e4e8b16335edb685eff71561a0e9bb685d7d28fd
TB831 <druu831@gmail.com>
2018-08-10 11:47:03 -0700

    sherlockAndTheValidString setup

---
+++ string manipulation/sherlockAndTheValidString/index.js
@@ -0,0 +1,7 @@
+// --- Directions
+// Hackerank Problem: Sherlock and The Valid String
+// link: https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+
+function isValid(s) {
+
+}
