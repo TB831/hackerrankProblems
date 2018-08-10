498500cc90be63a931616372f4ce8eec4d19cd47
TB831 <druu831@gmail.com>
2018-08-10 11:47:31 -0700

    sherlockAndTheValidString complete

--- string manipulation/sherlockAndTheValidString/index.js
+++ string manipulation/sherlockAndTheValidString/index.js
@@ -3,5 +3,15 @@
 // link: https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
 
 function isValid(s) {
-
+    const chars = {};
+    s.split('').map(char => {
+        chars[char] = chars[char] + 1 || 1;
+    })
+    for (let value in chars) {
+        if (chars[value] < 2) {
+            return 'NO';
+            break;
+        }
+    }
+    return 'YES';
 }
